ee59ce5d1c454205f32a000d6f942048e6340a06
Marcin Nowacki <nowacki.marcin@gmail.com>
2023-10-27 12:41:29 +0200

    Initial commit

---
+++ MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiApp2;
+
+public partial class MainPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+        BindingContext = this;
+    }
+
+    public Color FrameBackgroundColor { get; } = Colors.Azure;
+}
